b45c62e2955b5247737bcfb842fbea33d56437d9
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2023-01-22 06:23:25 -0500

    remove unnecessary IDs in HTML and fix input since it was not populating

--- server.js
+++ server.js
@@ -32,5 +32,24 @@ let data = {};
 let acceptData = () => {
   data["text"] = input.value
   console.log(data)
+
+  //I added createPost function below so when acceptData is fired, so is create post. Since, of course, when my object takes in data, I want it to simultaneously inject the html + template literal into my post section 
+  createPost()
+}
+
+//post creation - the template literal will input the data that goes into my object inside of the html designated below
+
+let createPost = () => {
+    posts.innerHTML += `
+    <div>
+    <p>${data.text}</p>
+    <span class="options"
+    <i onClick="editPost(this)" class="fas fa-edit"></i>
+      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+    </span>
+    </div>
+  `
+  //the input.value is set to blank after the post is created
+  input.value = ""
 }
 
