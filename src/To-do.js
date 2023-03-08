47f0e05793d6e219866fe7455d6c35e06ad4edd3
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2023-03-08 10:16:19 -0500

    update to do component with new interface and add useState hook

--- src/To-do.js
+++ src/To-do.js
@@ -1,6 +1,24 @@
-import './App.css';
+import React, { useState } from 'react';
+
 
 function Todo() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  function handleAddTodo() {
+    if (inputValue) {
+      setTodos([...todos, { id: Date.now(), text: inputValue }]);
+      setInputValue('');
+    }
+  }
+
+  function handleDeleteTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
   return (
     <div>
       <h1>Shopping list</h1>
@@ -8,9 +26,16 @@ function Todo() {
 
             <div className="left">
                 <form id="form">
-                    <label for="post">What do we need to buy?</label>
+                    <label>What do we need to buy?</label>
                     <br />
-                    <textarea name="post" id="input" cols="30" rows="10"></textarea>
+                    <textarea 
+                    type="text" 
+                    placeholder="Add a new to-do..."
+                    value={inputValue} 
+                    onChange={handleInputChange} 
+                    name="post" 
+                    id="input" 
+                    cols="30" rows="10"></textarea>
                     <br />
                     <div id="msg"></div>
                     <button type="submit">Post</button>
@@ -19,21 +44,16 @@ function Todo() {
             <div className="right">
                 <h3>List</h3>
                 <div id="posts">
-                    <div>
-                        <p></p>
-                        <span class="options">
-                            <i class="fas fa-edit"></i>
-                            <i class="fas fa-trash-alt"></i>
-                        </span>
-                    </div>
+                    <ul>
+                        {todos.map((todo) => (
+                            <li key={todo.id}>
+                            {todo.text}
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
 
-                    <div>
-                        <p></p>
-                        <span class="options">
-                            <i class="fas fa-edit"></i>
-                            <i class="fas fa-trash-alt"></i>
-                        </span>
-                    </div>
+                    
                 </div>
           </div>
         </div>
